8eda5192b486de6bc49d9ec3845cfd3ffbfe0aec
VincyaneBadouard <vincyane.badouard@gmail.com>
2024-02-14 15:26:34 +0100

    finish to recode echoes and gpsttime on UAV data

--- LiDAR_scripts/Derived variables/tests.R
+++ LiDAR_scripts/Derived variables/tests.R
@@ -12,10 +12,13 @@ data
 
 data <- unique(setorder(data, gpstime,Ring,Range,Patate)) 
 
-# microbenchmark(
+microbenchmark(
   data[, Patate := frank(Range), by = .(gpstime, Ring)]
-# ) # min = 893.6 mean = 1024.839  max = 1862.7 microsec (+ rapide)
+) # min = 893.6 mean = 1024.839  max = 1862.7 microsec (+ rapide)
 
+microbenchmark(
+  lasf@data[order(Range), ReturnNumber := 1:.N, by = .(gpstime, Ring)]
+)
 
 # Paraleliisation --------------------------------------------------------------
 
@@ -30,17 +33,17 @@ doSNOW::registerDoSNOW(cl)
 # microbenchmark(
 # registerDoParallel()
 foreach::foreach(
-    i=1:j, 
-    .packages = c("data.table")) %dopar% {
-      datas[[i]][, Patate := frank(Range), by = .(gpstime, Ring)]
-      cat(names(datas[[i]]))
-      
-      # Specify the file name for each i
-      output_file <- paste0("//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/HovermapUAV2023/P16_C14C15/Cor", i, ".csv")
-
-      # Write the modified dataset to a CSV file
-      fwrite(datas[[i]], file = output_file)
-    }
+  i=1:j, 
+  .packages = c("data.table")) %dopar% {
+    datas[[i]][, Patate := frank(Range), by = .(gpstime, Ring)]
+    cat(names(datas[[i]]))
+    
+    # Specify the file name for each i
+    output_file <- paste0("//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/HovermapUAV2023/P16_C14C15/Cor", i, ".csv")
+    
+    # Write the modified dataset to a CSV file
+    fwrite(datas[[i]], file = output_file)
+  }
 # , unit="microseconds") #  min 9.0171 mean = 17.41185 max = 692.1256 milisec
 stopCluster(cl)
 
