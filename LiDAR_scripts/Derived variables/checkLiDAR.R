544c77fc7de6edf1a6bbb2cb173b3f9f0bb7e5e2
VincyaneBadouard <vincyane.badouard@gmail.com>
2024-04-15 19:41:22 +0200

    conf figures

--- LiDAR_scripts/Derived variables/checkLiDAR.R
+++ LiDAR_scripts/Derived variables/checkLiDAR.R
@@ -1,7 +1,7 @@
 library(lidR)
 library(data.table)
 
-ST <- readLAS("//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/lowAltitudeFlight/LowFlight_alt4ha_buff100m_2023_RefAsInt.laz")
+ST <- readLAS("//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/HighAltitudeFlight/HighFlight_alt4ha_buff100m_2023_RefAsInt.laz")
 #"//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/HighAltitudeFlight/HighFlight_alt4ha_buff100m_2023_RefAsInt.laz"
 # "//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/lowAltitudeFlight/LowFlight_alt4ha_buff100m_2023_RefAsInt.laz"
 # "//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2022/P16_2022_4ha_buffer.laz"
@@ -31,7 +31,16 @@ tan(divergence*10^-3)*h # Footprint size in m
 
 # Penetration (proportion de points sol dans le dernier écho)
 table(ST@data$NumberOfReturns) # 14 echos ALS 2023 LowAlt; 12 HighAlt
-table(ST@data[ReturnNumber==11 & NumberOfReturns==11, Classification]) # 2 = sol
+min(ST@data$Z) # High : -2.34 ; Low : 4.06
+(nrow(ST@data[ReturnNumber==NumberOfReturns & Classification==2,])/nrow(ST@data))*100 # 2 = sol # High: 0.8% ; Low:2.7% of ground points in the last echo
+alt = 1000 # 1000 ; 500
+alt-mean(ST@data[ReturnNumber==NumberOfReturns & Classification==2,Z]) # High: 987 ; Low: 487 m distance
+range(ST@data[,Intensity]) # High: [1;625] ; Low: [0;29] of intensity
+mean(ST@data[ReturnNumber==NumberOfReturns & Classification==2,Intensity]) # High: 8.49 ; Low:1.3 of intensity in average at the ground
+mean(ST@data[Classification==2,Intensity]) # High: 8.6 ; Low: 1.3 of intensity in average at the ground
+mean(ST@data[ReturnNumber==1,Intensity]) # High: 22.1 ; Low: 5.8 of intensity in average in the first echo
+mean(ST@data[ReturnNumber==1,Intensity])-mean(ST@data[ReturnNumber==NumberOfReturns & Classification==2,Intensity]) # High: 13.6 ; Low: 4.5 of intensity lost
+(mean(ST@data[ReturnNumber==NumberOfReturns & Classification==2,Intensity])/mean(ST@data[ReturnNumber==1,Intensity]))*100 # High: 38.5% ; Low: 22% of intensity lost
 
 # gpstime
 range(ST@data$gpstime) # 354557511 354559330
