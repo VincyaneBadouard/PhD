bb5497a745bb7911a6a79e317ac0b2826953aa4c
VincyaneBadouard <vincyane.badouard@gmail.com>
2024-06-28 18:16:35 +0200

    Work on Intensity 1m - LAI2200

---
+++ LiDAR_scripts/Functions/TranslateLaz.R
@@ -0,0 +1,40 @@
+#' Translate LiDAR point cloud with a translation matrix
+#'
+#' @param las Point Cloud (laz)
+#' @param matrix Translation matrix ( 4 rows, 4 columns) (matrix)
+#'               (can be computed on CloudCompare)
+#'
+#' @return Translated point cloud (laz)
+#' @export
+#'
+#' @import data.table
+#'
+#' @examples
+#' library(lidR)
+#' las <- readLAS("//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/HovermapUAV2023/P16_C19C20/Split2/UAV_split_8295553.laz")
+#' matrix <- as.matrix(read.table("//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/HovermapUAV2023/Translation/Translation_matrix/Matrix_C19C20_cor.mat.txt", header=F))
+#' Trans <- TranslateLaz(las, matrix)
+#' 
+TranslateLaz <- function(las, matrix){
+  
+  las@data <- setDT(las@data)
+  
+  setnames(las@data, c("X", "Y", "Z"), c("x", "y", "z"))  # change names of local coordinates
+  
+  dat <- las@data[,.(x,y,z)] # only coordinates
+  dat$C <- 1 # 4 columns
+  dat <- as.matrix(dat)
+  
+  Trans <- dat %*% t(matrix) # pour appliquer la matrice de translation
+
+  rm(dat); gc()
+  # options(digits = 22)
+  las@data <- cbind(Trans[,1:3], las@data)
+  # View(las@data)
+  
+  setnames(las@data, c("V1", "V2", "V3"), c("X", "Y", "Z"))  # change names of local coordinates
+
+  
+  return(las)
+}
+
