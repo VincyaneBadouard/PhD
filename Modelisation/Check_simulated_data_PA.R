ce6b4b153b2c0fceb64e728cfc27f0ddd21b0e6e
VincyaneBadouard <vincyane.badouard@gmail.com>
2025-02-04 17:42:01 -0300

    Real species fit (4 sp)

--- Modelisation/Check_simulated_data_PA.R
+++ Modelisation/Check_simulated_data_PA.R
@@ -1,32 +1,97 @@
 
+
 # Check simulate data for pseudo-absences tests
 
-load("./Simdata/simulatedata_PA.Rdata")
+library(tidyverse)
+
+load("~/PhD/R_codes/PhD/Modelisation/Simdata/simulatedata_PA.Rdata")
 
 # plot initial total and subsampled probabilites, and presences as density plots
 
-test <- bind_rows(plist, .id = "Simulation") %>% 
+data <- bind_rows(plist, .id = "Simulation") %>% 
   rename(TotProbability= Probability) %>% 
   mutate(Probability = ifelse(!is.na(Presence), TotProbability, NA))
 
-test$TotPresence ==test$Presence
+gc()
+# Plot initial total and subsampled probabilites  ------------------------------
+plist <- vector('list', length(unique(data$Simulation)))
 
-test %>% 
-  filter(Simulation == "concavebell-30-30" | Simulation == "concavebell-30-1000") %>% 
-  ggplot(aes(x= logTransmittance, y= TotPresence)) +
-  # Total
-  # geom_point() +
-  # geom_line(aes(y = TotProbability), linewidth =1) + # total proba
-  
-  # Subsampled
-  geom_point(aes(y= Presence), col = "red") +
-  geom_line(aes(y = Probability), col = "red", linewidth =1) + 
-  
-  
-  theme_minimal() +
-  facet_wrap(~ Simulation, scales = "free")
+for(p in 1:length(unique(data$Simulation))){
+  # message(s)
+  plist[[p]] <- local({
+    
+    dataS <- data %>% 
+      filter(Simulation==unique(data$Simulation)[p])
+    
+    ggplot(dataS, aes(x= logTransmittance, y= TotPresence)) +
+      # Total
+      geom_point(col = "grey") +
+      geom_point(aes(y = TotProbability), col = "grey", size =1) + 
+      
+      ggridges::geom_density_ridges(data = (dataS %>% filter(TotPresence==1)), # Presence density
+                                    scale = 0.6,
+                                    jittered_points = T,
+                                    col = "grey",
+                                    alpha=0.05, point_alpha  = 0.7) +
+      
+      
+      # Subsampled
+      geom_point(aes(y= Presence), col = "red") +
+      geom_point(aes(y = Probability), col = "red", size =1) + 
+      
+      ggridges::geom_density_ridges(data = (dataS %>% filter(!is.na(Presence) & Presence==1)), # Presence density
+                                    scale = 0.6,
+                                    jittered_points = T,
+                                    col = "red",
+                                    alpha=0.05, point_alpha  = 0.7) +
+      
+      
+      labs(title = unique(dataS$Simulation), y="Presence") +
+      
+      theme_minimal()
+    
+  })
+}
 
+plots <- gridExtra::marrangeGrob(plist, nrow = 3, ncol = 3)
+# plots
 
-# labs(title=paste(Simulation,
-#                  "; a=",round(a,1), "b=",b, "c=",c,"N1=",N1,"N2=",N2),
-#      y="Presence (1) / Absence (0)", x="Explicative variable (X)")
+ggsave("SimulatedData_PA.pdf",
+       path = "D:/Mes Donnees/PhD/R_codes/PhD/Modelisation/Plots",
+       plots, width = 15, height = 10)
+
+gc()
+# Plot presence as density + points --------------------------------------------
+plist <- vector('list', length(unique(data$Simulation)))
+
+for(p in 1:length(unique(data$Simulation))){
+  # message(s)
+  plist[[p]] <- local({
+    
+    dataS <- data %>%
+      select(Simulation, Presence, logTransmittance) %>% 
+      filter(Simulation==unique(data$Simulation)[p]) %>% 
+      filter(Presence==1)
+    
+    
+    ggplot(dataS, aes(x= logTransmittance, y= Simulation)) +
+      ggridges::geom_density_ridges(scale = 0.6,
+                                    jittered_points = T,
+                                    alpha=0, point_alpha  = 0.7) +
+      
+      theme_classic() +
+      theme(axis.text.y = element_blank()) +
+      guides(fill = FALSE) +
+      labs(title = unique(dataS$Simulation),
+           x= "logTransmittance", y="Density") 
+    
+  })
+}
+
+plots <- gridExtra::marrangeGrob(plist, nrow = 2, ncol = 3)
+# plots
+
+ggsave("SimulatedData_PA_density_points.pdf",
+       path = "D:/Mes Donnees/PhD/R_codes/PhD/Modelisation/Plots",
+       plots, width = 15, height = 10)
+
