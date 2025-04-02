7cadc71394b1cbfcf31d09b84756c452e5ec7e01
Vincyane BADOUARD <vincyane.badouard@gmail.com>
2025-04-02 18:14:41 +0200

    BIC plots, bind stakes measurments, explo attenuation prblm

--- LiDAR_scripts/Derived variables/AMAPVox_relative/Crop_voxelspace.R
+++ LiDAR_scripts/Derived variables/AMAPVox_relative/Crop_voxelspace.R
@@ -3,16 +3,19 @@ library(AMAPVox)
 
 
 VX <- AMAPVox::readVoxelSpace(
-  "//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/HighAltitudeFlight/ForTrees/Vox/P16_2023_HighAlt_25ha_buffer_PadHLE_intensity1m_testnewEB.vox"
+  "//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/HighAltitudeFlight/ForTrees/Vox/P16_2023_HighAlt_4ha_buffer_PadHLE_intensity1m_testnewEB.vox"
 )
+VX <- vxsp
 # View(VX@data)
 AMAPVox::plot(VX, variable.name = "ground_distance") 
 
-range(VX@data$i); range(VX@data$j) ; range(VX@data$k) # 0 699
+range(VX@data$i); range(VX@data$j) ; range(VX@data$k) # 0 700
 
 VX_crop <- AMAPVox::crop(VX, imin = 100, imax = 200, jmin = 100, jmax = 200) 
 
-AMAPVox::plot(VX_crop, variable.name = "ground_distance") 
+range(VX_crop@data$i); range(VX_crop@data$j) ; range(VX_crop@data$k) # 0 700
+
+AMAPVox::plot(VX_crop) 
 
 writeVoxelSpace(VX_crop,  
                 "//amap-data.cirad.fr/work/users/VincyaneBadouard/Lidar/ALS2023/HighAltitudeFlight/ForTrees/Vox/P16_2023_HighAlt_1ha_buffer_PadHLE_intensity1m_testnewEB.vox"
